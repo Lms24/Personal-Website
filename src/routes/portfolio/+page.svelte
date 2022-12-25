efa2031ead9f1be19dd7c803a0e545e8ab470ddf
Lukas Stracke <lukas.stracke@sentry.io>
2022-12-25 10:46:03 +0100

    textlink adjustments

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -37,19 +37,18 @@
     <Sentry slot="image" classes="flex h-48 w-48" />
     <div slot="description">
       <p>
-        Sentry is an error and performance monitoring service that puts developers first.<TextLink
+        Sentry is an error and performance monitoring service that puts developers first. <TextLink
           href="https://sentry.io/welcome"
           bold>Sentry</TextLink
-        >tries to gather everything developers need to know about why their application crashed or
-        is slow.
+        > tries to gather everything developers need to know about why their application crashed or is
+        slow.
       </p>
       <p>
         Sentry's SDKs are the gateway to this service. Everyone who wants to use Sentry needs to add
-        an SDK to their application. I maintain<TextLink
+        an SDK to their application. I maintain <TextLink
           bold
           href="https://github.com/getsentry/sentry-javascript">Sentry's JavaScript SDKs</TextLink
-        >which are 15 SDKs supporting all popular frontend and backend frameworks based on
-        JavaScript.
+        > which are 15 SDKs supporting all popular frontend and backend frameworks based on JavaScript.
       </p>
     </div>
   </PortfolioItem>
@@ -65,17 +64,16 @@
     <div slot="description">
       <p>
         Henri is an app for professional and volunteering EMTs and paramedics to track shifts and
-        calls. By keeping track of shifts and calls,<TextLink href="https://henri.live" bold
+        calls. By keeping track of shifts and calls, <TextLink href="https://henri.live" bold
           >Henri</TextLink
-        >users receive statistics about their work, allowing insights into the nature of their work
+        > users receive statistics about their work, allowing insights into the nature of their work
         or hobby.
       </p>
       <p>
-        Henri was intially started by<TextLink bold href="https://markusdeutsch.at"
+        Henri was intially started by <TextLink bold href="https://markusdeutsch.at"
           >Markus Deutsch</TextLink
-        >who builds and maintains the Android app. I joined the project to build a
-        platform-independent progressive web app. It is based on Angular and relies on various
-        Firebase (BaaS) services.
+        > who builds and maintains the Android app. I joined the project to build a platform-independent
+        progressive web app. It is based on Angular and relies on various Firebase (BaaS) services.
       </p>
     </div>
   </PortfolioItem>
@@ -87,10 +85,10 @@
 
     <div slot="description">
       <p>
-        JSR, short for<TextLink href="https://github.com/lms24/jsr" bold
-          >Java Test Suite Reduction Framework</TextLink
-        > is a tool for reducing the size of test suits in Java projects. This tool can be used to identify
-        and eliminate redundant or obsolete test cases in a test suite.
+        JSR, short for <TextLink href="https://github.com/lms24/jsr" bold
+          >Java Test Suite Reduction Framework
+        </TextLink> is a tool for reducing the size of test suits in Java projects. This tool can be
+        used to identify and eliminate redundant or obsolete test cases in a test suite.
       </p>
       <p>
         I created JSR while working over the course of my Master's thesis in which I explored test
@@ -108,9 +106,9 @@
 
     <div slot="description">
       <p>
-        I created<TextLink href="https://citeworks-bf003.web.app/home" bold>CiteWorks</TextLink>for
-        a university course about interactive data visualization of digital libraries. This app
-        visualizes citation and co-authorship networks amongst academic publications.
+        I created <TextLink href="https://citeworks-bf003.web.app/home" bold>CiteWorks</TextLink> for
+        a university course about interactive data visualization of digital libraries. This app visualizes
+        citation and co-authorship networks amongst academic publications.
       </p>
       <p>
         CiteWorks can be used to analyze publication habits of academic authors and to identify
